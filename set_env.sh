90635d10ab54da26bf4248cd959cbb7844ead5ec
Dan Ackerson <dan@ackerson.de>
2017-03-15 11:49:02 +0100

    wrong place

--- set_env.sh
+++ set_env.sh
@@ -5,7 +5,7 @@ export consolePasswdHash=$(echo $encodedConsolePasswdHash | base64 --decode)
 sed -i -e "s@{{login_ssh_pubkey}}@$doSSHPubKey@" digitalocean_ignition.json
 sed -i -e "s@{{circleci_deploy_pubkey}}@$circleCIDeployPubKey@" digitalocean_ignition.json
 sed -i -e "s/{{console_passwd_hash}}/$consolePasswdHash/" digitalocean_ignition.json
-sed -i -e "s/{{deploy_user}}/$deployUser/g" sshd_config
+sed -i -e "s/{{deploy_user}}/$deployUser/" sshd_config
 export encodedSSHDConfig=$(base64 -w 0 sshd_config)
-sed -i -e "s/{{deploy_user}}/$deployUser/" digitalocean_ignition.json
+sed -i -e "s/{{deploy_user}}/$deployUser/g" digitalocean_ignition.json
 sed -i -e "s/{{encoded_sshd_config}}/$encodedSSHDConfig/" digitalocean_ignition.json
