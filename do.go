e10204d7407410d62d82ffe5a6ef3408ec1c4d4d
Dan Ackerson <dan@ackerson.de>
2017-04-16 09:59:20 +0200

    create and migrate DNS entries of new ackerson.de server (#2)
    * testing new mega deploy
    
    * gotta parse
    
    * gotta parse
    
    * gotta parse correctly
    
    * correct call to other circleCI builds
    
    * again
    
    * wait until DO has the IP addy
    
    * spacing in logs
    
    * full deploy test
    
    * work with dropletID param to print droplet info
    
    * move floatingIP to new instance
    
    * more sleep\!
    
    * body parts rain down
    
    * update IP addys
    
    * generate SSL cert on new machine
    
    * setup ssh key and wait for droplet to get IPv6 addr
    
    * trying something diff
    
    * if droplet has pending actions, just wait til complete
    
    * another idea
    
    * hmmmm
    
    * no tty
    
    * again with verve
    
    * again
    
    * move new server migration to master branch

--- do.go
+++ do.go
@@ -1,17 +1,20 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
 	"strconv"
+	"strings"
+	"time"
 
 	"github.com/digitalocean/godo"
 	"golang.org/x/oauth2"
 )
 
 var doDropletInfoSite = "https://cloud.digitalocean.com/droplets/"
-
+var floatingIPAddress = "138.68.115.62"
 var encodedDOSSHLoginPubKey = os.Getenv("encodedDOSSHLoginPubKey")
 var doPersonalAccessToken = os.Getenv("doPersonalAccessToken")
 var circleCIBuild = os.Getenv("CIRCLE_BUILD_NUM")
@@ -36,15 +39,86 @@ func main() {
 	oauthClient := oauth2.NewClient(oauth2.NoContext, tokenSource)
 	client := godo.NewClient(oauthClient)
 
-	droplets, err := DropletList(client)
-	if err != nil {
-		fmt.Printf("Failed to list droplets: %s", err)
-	}
-	for _, droplet := range droplets {
+	fnPtr := flag.String("fn", "updateDNS|createNewServer", "which function to run")
+	dropletIDPtr := flag.String("dropletID", "<digitalOceanDropletID>", "DO droplet to attach floatingIP to")
+	flag.Parse()
+	if *fnPtr == "createNewServer" {
+		droplet := createDroplet(client)
+		waitUntilDropletReady(client, droplet.ID)
+		droplet, _, _ = client.Droplets.Get(oauth2.NoContext, droplet.ID)
+
 		ipv4, _ := droplet.PublicIPv4()
 		addr := doDropletInfoSite + strconv.Itoa(droplet.ID)
 		fmt.Printf("%s: %s @%s\n", ipv4, droplet.Name, addr)
+
+		// Write /tmp/new_digital_ocean_droplet_params
+		envVarsFile := []byte("export NEW_SERVER_IPV4=" + ipv4 + "\nexport NEW_DROPLET_ID=" + strconv.Itoa(droplet.ID) + "\n")
+		err := ioutil.WriteFile("/tmp/new_digital_ocean_droplet_params", envVarsFile, 0644)
+		if err != nil {
+			fmt.Printf("Failed to write /tmp/new_digital_ocean_droplet_params: %s", err)
+		}
+	} else {
+		dropletID, _ := strconv.Atoi(*dropletIDPtr)
+		droplet, _, _ := client.Droplets.Get(oauth2.NoContext, dropletID)
+		fmt.Printf("\ngoing to work on DropletID: %d\n", droplet.ID)
+
+		reassignFloatingIP(client, droplet)
+		droplet, _, _ = client.Droplets.Get(oauth2.NoContext, dropletID)
+
+		// update ipv6 DNS entry to new droplet
+		ipv6, _ := droplet.PublicIPv6()
+		fmt.Printf("new IPv6 addr: %s\n", ipv6)
+		ackersonDERecordIDIPv6 := 23738236
+		record, _, _ := client.Domains.Record(oauth2.NoContext, "ackerson.de", ackersonDERecordIDIPv6)
+		fmt.Printf("current IPv6 %s: %s => %s", record.Name, record.Type, record.Data)
+
+		editRequest := &godo.DomainRecordEditRequest{
+			Type: record.Type,
+			Name: record.Name,
+			Data: strings.ToLower(ipv6),
+		}
+		_, _, err := client.Domains.EditRecord(oauth2.NoContext, "ackerson.de", ackersonDERecordIDIPv6, editRequest)
+		if err != nil {
+			fmt.Printf("FAIL domain update IPv6: %s\n", err)
+		}
 	}
+}
+
+// wait for in-progress actions to complete
+func waitUntilDropletReady(client *godo.Client, dropletID int) {
+	opt := &godo.ListOptions{}
+
+	for ready := false; !ready; {
+		actions, _, _ := client.Droplets.Actions(oauth2.NoContext, dropletID, opt)
+		ready = true
+		for j, action := range actions {
+			fmt.Printf("%d: %s => %s\n", j, action.Type, action.Status)
+			if action.Status == "in-progress" {
+				ready = false
+			}
+		}
+		if !ready {
+			time.Sleep(time.Second * 5)
+		}
+	}
+}
+
+func reassignFloatingIP(client *godo.Client, droplet *godo.Droplet) {
+	waitUntilDropletReady(client, droplet.ID)
+	client.FloatingIPActions.Unassign(oauth2.NoContext, floatingIPAddress)
+
+	waitUntilDropletReady(client, droplet.ID)
+	_, _, err := client.FloatingIPActions.Assign(oauth2.NoContext, floatingIPAddress, droplet.ID)
+	if err != nil {
+		fmt.Printf("error by assign: %s\n", err.Error())
+		client.FloatingIPActions.Assign(oauth2.NoContext, floatingIPAddress, droplet.ID)
+	}
+
+	waitUntilDropletReady(client, droplet.ID)
+}
+
+func createDroplet(client *godo.Client) *godo.Droplet {
+	var newDroplet *godo.Droplet
 
 	fingerprint := "e0:a3:4c:5a:5a:1b:9c:bb:b5:51:a7:7f:62:27:51:96"
 	dropletName := "b" + circleCIBuild + ".ackerson.de"
@@ -68,24 +142,17 @@ func main() {
 			UserData: string(digitaloceanIgnitionJSON),
 		}
 
-		//fmt.Printf("Droplet creation request: %v", createRequest)
-
-		newDroplet, _, err := client.Droplets.Create(oauth2.NoContext, createRequest)
+		newDroplet, _, err = client.Droplets.Create(oauth2.NoContext, createRequest)
 		if err != nil {
 			fmt.Printf("\nUnexpected ERROR: %s\n\n", err)
-			// TODO: set exit code to !0 (so CircleCI reports failed build)
 			os.Exit(1)
-		} else {
-			ipv4, _ := newDroplet.PublicIPv4()
-			addr := doDropletInfoSite + strconv.Itoa(newDroplet.ID)
-			fmt.Printf("\n%s: %s CREATED @ %s\n", ipv4, newDroplet.Name, addr)
-			os.Exit(0)
 		}
 	}
+
+	return newDroplet
 }
 
-// DropletList is now commented
-func DropletList(client *godo.Client) ([]godo.Droplet, error) {
+func dropletList(client *godo.Client) ([]godo.Droplet, error) {
 	// create a list to hold our droplets
 	list := []godo.Droplet{}
 
