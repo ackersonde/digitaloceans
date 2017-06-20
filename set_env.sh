e63f6b1870e6ceb7ce5740d21903c7ddfc5b722b
Dan Ackerson <dan@ackerson.de>
2017-06-20 10:31:00 +0200

    reference tmp files between workspaces

--- set_env.sh
+++ set_env.sh
@@ -2,10 +2,10 @@
 export doSSHPubKey=$(echo $encodedDOSSHLoginPubKey | base64 --decode)
 export circleCIDeployPubKey=$(echo $encodedCircleCIDeployPubKey | base64 --decode)
 export consolePasswdHash=$(echo $encodedConsolePasswdHash | base64 --decode)
-sed -i -e "s@{{login_ssh_pubkey}}@$doSSHPubKey@" digitalocean_ignition.json
-sed -i -e "s@{{circleci_deploy_pubkey}}@$circleCIDeployPubKey@" digitalocean_ignition.json
-sed -i -e "s/{{console_passwd_hash}}/$consolePasswdHash/" digitalocean_ignition.json
-sed -i -e "s/{{deploy_user}}/$deployUser/" sshd_config
-export encodedSSHDConfig=$(base64 -w 0 sshd_config)
-sed -i -e "s/{{deploy_user}}/$deployUser/g" digitalocean_ignition.json
-sed -i -e "s/{{encoded_sshd_config}}/$encodedSSHDConfig/" digitalocean_ignition.json
+sed -i -e "s@{{login_ssh_pubkey}}@$doSSHPubKey@" /tmp/digitalocean_ignition.json
+sed -i -e "s@{{circleci_deploy_pubkey}}@$circleCIDeployPubKey@" /tmp/digitalocean_ignition.json
+sed -i -e "s/{{console_passwd_hash}}/$consolePasswdHash/" /tmp/digitalocean_ignition.json
+sed -i -e "s/{{deploy_user}}/$deployUser/" /tmp/sshd_config
+export encodedSSHDConfig=$(base64 -w 0 /tmp/sshd_config)
+sed -i -e "s/{{deploy_user}}/$deployUser/g" /tmp/digitalocean_ignition.json
+sed -i -e "s/{{encoded_sshd_config}}/$encodedSSHDConfig/" /tmp/digitalocean_ignition.json
