a1138477729c1b6dadd2bd95f40a99d0f0fe3083
Dan Ackerson <dan@ackerson.de>
2018-12-25 09:10:09 +0100

    careful with bash vars

--- utils/addSSHipFW.sh
+++ utils/addSSHipFW.sh
@@ -1,3 +1,3 @@
 curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $digitalOceanToken" \
--d '{"inbound_rules":[{"protocol":"tcp","ports":"22","sources":{"addresses":"$public_ip_address"}}]}' \
+-d '{"inbound_rules":[{"protocol":"tcp","ports":"22","sources":{"addresses":"'+$public_ip_address+'"}}]}' \
 "https://api.digitalocean.com/v2/firewalls/$doFirewallID/rules"
