8e839eda88dce2567e3bcb7fa1b855f634c9564a
Dan Ackerson <dan@ackerson.de>
2018-12-25 13:34:39 +0100

    [skip ci] test

--- utils/addSSHipFW.sh
+++ utils/addSSHipFW.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-json='{"inbound_rules":[{"protocol":"tcp","ports":"22","sources":{"addresses":["'"$public_ip_address"'"]}}]}'
+json='{"inbound_rules":[{"protocol":"tcp","ports":"22","sources":{"addresses":["91.31.13.113"]}}]}'
 
 curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $digitalOceanToken" \
 -d $json "https://api.digitalocean.com/v2/firewalls/$doFirewallID/rules"
