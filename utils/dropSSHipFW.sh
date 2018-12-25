e825643132b1d9c39c07060d4d76f5a4ee66197b
Dan Ackerson <dan@ackerson.de>
2018-12-25 13:45:35 +0100

    try again

--- utils/dropSSHipFW.sh
+++ utils/dropSSHipFW.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# good luck
 public_ip_address=$(curl -s https://checkip.amazonaws.com)
 json='{"inbound_rules":[{"protocol":"tcp","ports":"22","sources":{"addresses":["'"$public_ip_address"'"]}}]}'
 
