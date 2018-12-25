99f3599a2e8f05112051cedf5c3d94a62ddb84ab
Dan Ackerson <dan@ackerson.de>
2018-12-25 09:40:18 +0100

    [skip ci] json curl fun

--- utils/addSSHipFW.sh
+++ utils/addSSHipFW.sh
@@ -1,3 +1,4 @@
+json='{"inbound_rules":[{"protocol":"tcp","ports":"22","sources":{"addresses":["'"$public_ip_address"'"]}}]}'
+
 curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer $digitalOceanToken" \
--d '{"inbound_rules":[{"protocol":"tcp","ports":"22","sources":{"addresses":"'+$public_ip_address+'"}}]}' \
-"https://api.digitalocean.com/v2/firewalls/$doFirewallID/rules"
+-d $json "https://api.digitalocean.com/v2/firewalls/$doFirewallID/rules"
