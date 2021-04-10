2f1679b7ba67ba337b006503c887d59c3c6dce98
danackerson <dan@ackerson.de>
2021-04-10 09:45:32 +0200

    move back to DO

--- do.go
+++ do.go
@@ -9,7 +9,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
-	"net"
 	"os"
 	"strconv"
 	"strings"
@@ -19,7 +18,6 @@ import (
 	"github.com/digitalocean/godo"
 	"golang.org/x/crypto/ssh"
 	"golang.org/x/net/context"
-	"golang.org/x/oauth2"
 )
 
 var doDropletInfoSite = "https://cloud.digitalocean.com/droplets/"
@@ -43,10 +41,12 @@ func main() {
 		droplet, _, _ = client.Droplets.Get(context.Background(), droplet.ID)
 
 		ipv4, _ := droplet.PublicIPv4()
+		ipv6, _ := droplet.PublicIPv6()
 
 		// Write /tmp/new_digital_ocean_droplet_params
 		envVarsFile := []byte(
 			"export NEW_SERVER_IPV4=" + ipv4 +
+				"\nexport NEW_SERVER_IPV6=" + ipv6 +
 				"\nexport NEW_DROPLET_ID=" + strconv.Itoa(droplet.ID) +
 				"\nexport NEW_SSH_KEY_ID=" + strconv.Itoa(key.ID))
 
@@ -56,14 +56,14 @@ func main() {
 		}
 
 		var firewallID = os.Getenv("CTX_DIGITALOCEAN_FIREWALL")
-		_, err2 := client.Firewalls.AddDroplets(oauth2.NoContext, firewallID, droplet.ID)
+		_, err2 := client.Firewalls.AddDroplets(context.Background(), firewallID, droplet.ID)
 		if err2 != nil {
 			fmt.Printf("Failed to add droplet to Firewall: %s", err2)
 		}
 	} else if *fnPtr == "deleteServer" {
 		dropletID, _ := strconv.Atoi(*dropletIDPtr)
 		keyID, _ := strconv.Atoi(*keyIDPtr)
-		droplet, _, _ := client.Droplets.Get(oauth2.NoContext, dropletID)
+		droplet, _, _ := client.Droplets.Get(context.Background(), dropletID)
 		fmt.Printf("\ndeleting DropletID: %d\n", droplet.ID)
 
 		common.DeleteDODroplet(dropletID)
@@ -73,20 +73,8 @@ func main() {
 	}
 }
 
-func getCurrentIP() string {
-	conn, err := net.Dial("udp", "8.8.8.8:80")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer conn.Close()
-
-	localAddr := conn.LocalAddr().(*net.UDPAddr)
-
-	return localAddr.IP.String()
-}
-
 func updateIPV6(client *godo.Client, ipv6 string, hostname string, domainID int) {
-	record, _, err := client.Domains.Record(oauth2.NoContext, hostname, domainID)
+	record, _, err := client.Domains.Record(context.Background(), hostname, domainID)
 	if err != nil {
 		log.Printf("unable to updateIPv6 for %s: %s", hostname, err.Error())
 	}
@@ -97,11 +85,11 @@ func updateIPV6(client *godo.Client, ipv6 string, hostname string, domainID int)
 		Name: record.Name,
 		Data: strings.ToLower(ipv6),
 	}
-	_, _, err = client.Domains.EditRecord(oauth2.NoContext, hostname, domainID, editRequest)
+	_, _, err = client.Domains.EditRecord(context.Background(), hostname, domainID, editRequest)
 	for err != nil {
 		fmt.Printf("FAIL domain update IPv6: %s\n", err)
 		time.Sleep(5 * time.Second)
-		_, _, err = client.Domains.EditRecord(oauth2.NoContext, hostname, domainID, editRequest)
+		_, _, err = client.Domains.EditRecord(context.Background(), hostname, domainID, editRequest)
 	}
 }
 
@@ -110,7 +98,7 @@ func waitUntilDropletReady(client *godo.Client, dropletID int) {
 	opt := &godo.ListOptions{}
 
 	for ready := false; !ready; {
-		actions, _, _ := client.Droplets.Actions(oauth2.NoContext, dropletID, opt)
+		actions, _, _ := client.Droplets.Actions(context.Background(), dropletID, opt)
 		ready = true
 		for j, action := range actions {
 			fmt.Printf("%d: %s => %s\n", j, action.Type, action.Status)
@@ -127,13 +115,13 @@ func waitUntilDropletReady(client *godo.Client, dropletID int) {
 }
 
 func reassignFloatingIP(client *godo.Client, droplet *godo.Droplet) {
-	client.FloatingIPActions.Unassign(oauth2.NoContext, common.FloatingIPAddress)
+	client.FloatingIPActions.Unassign(context.Background(), common.FloatingIPAddress)
 
-	_, _, err := client.FloatingIPActions.Assign(oauth2.NoContext, common.FloatingIPAddress, droplet.ID)
+	_, _, err := client.FloatingIPActions.Assign(context.Background(), common.FloatingIPAddress, droplet.ID)
 	for err != nil {
 		fmt.Printf("WARN: %s\n", err.Error())
 		time.Sleep(5 * time.Second)
-		_, _, err = client.FloatingIPActions.Assign(oauth2.NoContext, common.FloatingIPAddress, droplet.ID)
+		_, _, err = client.FloatingIPActions.Assign(context.Background(), common.FloatingIPAddress, droplet.ID)
 	}
 }
 
@@ -156,7 +144,7 @@ func createSSHKey(client *godo.Client) *godo.Key {
 
 	log.Println("Public key generated")
 
-	key, _, err := client.Keys.Create(oauth2.NoContext, createRequest)
+	key, _, err := client.Keys.Create(context.Background(), createRequest)
 	if err != nil {
 		log.Printf("Keys.Create returned error: %v", err)
 	} else {
@@ -201,7 +189,7 @@ func createDroplet(client *godo.Client, key *godo.Key) *godo.Droplet {
 			UserData: string(digitaloceanIgnitionJSON),
 		}
 
-		newDroplet, _, err = client.Droplets.Create(oauth2.NoContext, createRequest)
+		newDroplet, _, err = client.Droplets.Create(context.Background(), createRequest)
 		if err != nil {
 			fmt.Printf("\nUnexpected ERROR: %s\n\n", err)
 			os.Exit(1)
--- do_ubuntu_userdata.sh
+++ do_ubuntu_userdata.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
+
+# TODO: replace this key w/ 30 day rotation one
 cat > /root/.ssh/id_rsa <<EOF
 $CTX_RASPBERRYPI_SSH_PRIVKEY
 EOF
 
 chmod 400 /root/.ssh/id_rsa
 touch ~/.hushlogin
+
+# TODO: install docker engine
+apt update && apt install apt-transport-https ca-certificates curl software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt update && apt install docker-ce
+
+apt upgrade -y
+
+# TODO: setup ipv6 capability?
+#cat > /etc/docker/daemon.json <<EOF
+# {
+#  "ipv6": true,
+#  "fixed-cidr-v6": "<DO_RANGE>:ffff::/80"
+#}
+#EOF
+#systemctl restart docker
+
+#ip6tables -t nat -A POSTROUTING -s <DO_RANGE>:ffff::/80 ! -o docker0 -j MASQUERADE
