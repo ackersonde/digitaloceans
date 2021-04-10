d096bf88a3aa00d1a60b50327008f4295b327794
danackerson <dan@ackerson.de>
2021-04-10 13:03:36 +0200

    and now

--- do_ubuntu_userdata.sh
+++ do_ubuntu_userdata.sh
@@ -9,17 +9,15 @@ chmod 400 /root/.ssh/id_rsa
 touch ~/.hushlogin
 
 # Setup Advanced DO monitoring
-curl -fsSL https://repos.insights.digitalocean.com/sonar-agent.asc | gpg --dearmor | tee /usr/share/keyrings/digitalocean-archive-keyring.gpg
-add-apt-repository "deb [signed-by=/usr/share/keyrings/digitalocean-archive-keyring.gpg] https://repos.insights.digitalocean.com/apt/do-agent/ main main"
-
-# Install docker engine
-#apt update && apt install apt-transport-https ca-certificates curl software-properties-common
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor | tee /usr/share/keyrings/docker-archive-keyring.gpg
-#add-apt-repository "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu focal stable"
+echo "deb https://repos.insights.digitalocean.com/apt/do-agent/ main main" > /etc/apt/sources.list.d/digitalocean.list
+curl -fsSL https://repos.insights.digitalocean.com/sonar-agent.asc | apt-key add -
 
 apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install docker-ce do-agent
+DEBIAN_FRONTEND=noninteractive apt-get install docker.io do-agent
 DEBIAN_FRONTEND=noninteractive apt-get upgrade
+
+systemctl start docker
+systemctl enable docker
 
 # TODO: setup ipv6 capability?
 #cat > /etc/docker/daemon.json <<EOF
