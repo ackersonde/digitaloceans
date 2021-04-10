fbaba3eb819f3c0c616a1a3b94471c2c36273f68
danackerson <dan@ackerson.de>
2021-04-10 12:49:26 +0200

    fun

--- do_ubuntu_userdata.sh
+++ do_ubuntu_userdata.sh
@@ -9,16 +9,17 @@ chmod 400 /root/.ssh/id_rsa
 touch ~/.hushlogin
 
 # Setup Advanced DO monitoring
-curl -fsSL https://repos.insights.digitalocean.com/sonar-agent.asc | gpg --dearmor | tee /usr/share/keyrings/sonar-agent-keyring.gpg
-add-apt-repository "deb https://repos.insights.digitalocean.com/apt/do-agent/ main main"
+curl -fsSL https://repos.insights.digitalocean.com/sonar-agent.asc | gpg --dearmor | tee /usr/share/keyrings/digitalocean-archive-keyring.gpg
+add-apt-repository "deb [signed-by=/usr/share/keyrings/digitalocean-archive-keyring.gpg] https://repos.insights.digitalocean.com/apt/do-agent/ main main"
 
 # Install docker engine
 #apt update && apt install apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor | tee /usr/share/keyrings/docker-keyring.gpg
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor | tee /usr/share/keyrings/docker-archive-keyring.gpg
+#add-apt-repository "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu focal stable"
 
-apt-get update && apt-get install -y docker-ce do-agent
-apt-get upgrade -y
+apt-get update
+DEBIAN_FRONTEND=noninteractive apt-get install docker-ce do-agent
+DEBIAN_FRONTEND=noninteractive apt-get upgrade
 
 # TODO: setup ipv6 capability?
 #cat > /etc/docker/daemon.json <<EOF
