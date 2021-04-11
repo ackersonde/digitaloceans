90aa01f8ecbd11c07ccd8c47c95f85ee0c3fb73f
danackerson <dan@ackerson.de>
2021-04-11 16:13:10 +0200

    enable unattended auto upgrades

--- do_ubuntu_userdata.sh
+++ do_ubuntu_userdata.sh
@@ -18,6 +18,29 @@ apt-get -y install docker.io do-agent
 systemctl start docker
 systemctl enable docker
 
+echo unattended-upgrades unattended-upgrades/enable_auto_updates boolean true | debconf-set-selections
+dpkg-reconfigure -f noninteractive unattended-upgrades
+
+cat > /etc/apt/apt.conf.d/50unattended-upgrades << EOF
+// Automatically upgrade packages from these (origin, archive) pairs
+Unattended-Upgrade::Allowed-Origins {
+    // ${distro_id} and ${distro_codename} will be automatically expanded
+    "${distro_id} stable";
+    "${distro_id} ${distro_codename}-security";
+    "${distro_id} ${distro_codename}-updates";
+//  "${distro_id} ${distro_codename}-proposed-updates";
+};
+
+// Do automatic removal of new unused dependencies after the upgrade
+// (equivalent to apt-get autoremove)
+Unattended-Upgrade::Remove-Unused-Dependencies "true";
+
+// Automatically reboot *WITHOUT CONFIRMATION* if a
+// the file /var/run/reboot-required is found after the upgrade
+Unattended-Upgrade::Automatic-Reboot "true";
+EOF
+
+
 # TODO: setup ipv6 capability?
 #cat > /etc/docker/daemon.json <<EOF
 # {
