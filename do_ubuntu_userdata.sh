320acfa299957c8d541c4a7a8e25dd1bfb26f679
danackerson <dan@ackerson.de>
2021-04-11 10:25:30 +0200

    try dist-upgrade

--- do_ubuntu_userdata.sh
+++ do_ubuntu_userdata.sh
@@ -9,12 +9,12 @@ chmod 400 /root/.ssh/id_rsa
 touch ~/.hushlogin
 
 # Setup Advanced DO monitoring
-echo "deb https://repos.insights.digitalocean.com/apt/do-agent/ main main" > /etc/apt/sources.list.d/digitalocean.list
+echo "deb https://repos.insights.digitalocean.com/apt/do-agent/ main main" > /etc/apt/sources.list.d/digitalocean-agent.list
 curl -fsSL https://repos.insights.digitalocean.com/sonar-agent.asc | apt-key add -
 
 apt-get update
 apt-get -y install docker.io do-agent
-apt-get --yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade
+apt-get --yes -o Dpkg::Options::="--force-confold" dist-upgrade
 
 systemctl start docker
 systemctl enable docker
