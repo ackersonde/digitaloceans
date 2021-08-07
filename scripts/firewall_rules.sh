71b019ac9dac12886a55c5074aacbb817a756bfb
danackerson <dan@ackerson.de>
2021-08-07 09:42:56 +0200

    wake build server before work [skip ci]

--- scripts/firewall_rules.sh
+++ scripts/firewall_rules.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 SERVERS="ubuntu@$CTX_MASTER_HOST ubuntu@$CTX_SLAVE_HOST ackersond@$CTX_BUILD_HOST"
 
+# login to the master and run WAKE_ON_LAN on build host, wait 10 seconds and proceed
+ssh -o StrictHostKeyChecking=no ubuntu@$CTX_MASTER_HOST "wakeonlan 2c:f0:5d:5e:84:43"
+sleep 10
+
 for i in $SERVERS
 do
    ssh -o StrictHostKeyChecking=no $i \
