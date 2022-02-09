1d22bb17b371cb34336405718a23e4ad9160782d
Dan Ackerson <dan@ackerson.de>
2022-02-09 13:47:28 +0100

    revert ip6tables [skip ci]

--- scripts/firewall_rules.sh
+++ scripts/firewall_rules.sh
@@ -2,9 +2,7 @@
 SERVERS="ubuntu@$CTX_MASTER_HOST ubuntu@$CTX_SLAVE_HOST ackersond@$CTX_BUILD_HOST"
 
 # login to the master and run WAKE_ON_LAN on build host, wait 10 seconds and proceed
-ssh -o StrictHostKeyChecking=no ubuntu@$CTX_MASTER_HOST "wakeonlan 2c:f0:5d:5e:84:43 && \
-sudo ip6tables -A INPUT -s $NEW_SERVER_IPV6 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT && \
-sudo ip6tables -D INPUT -s $OLD_SERVER_IPV6 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT"
+ssh -o StrictHostKeyChecking=no ubuntu@$CTX_MASTER_HOST "wakeonlan 2c:f0:5d:5e:84:43"
 
 sleep 10
 
