517ade57d7f77edbad6f5642b3cbb1f1fbaaa67c
Dan Ackerson <dan@ackerson.de>
2022-02-09 11:56:55 +0100

    need to update ip6tables as well

--- scripts/firewall_rules.sh
+++ scripts/firewall_rules.sh
@@ -2,7 +2,10 @@
 SERVERS="ubuntu@$CTX_MASTER_HOST ubuntu@$CTX_SLAVE_HOST ackersond@$CTX_BUILD_HOST"
 
 # login to the master and run WAKE_ON_LAN on build host, wait 10 seconds and proceed
-ssh -o StrictHostKeyChecking=no ubuntu@$CTX_MASTER_HOST "wakeonlan 2c:f0:5d:5e:84:43"
+ssh -o StrictHostKeyChecking=no ubuntu@$CTX_MASTER_HOST "wakeonlan 2c:f0:5d:5e:84:43 && \
+sudo ip6tables -A INPUT -s $NEW_SERVER_IPV6 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT && \
+sudo ip6tables -D INPUT -s $OLD_SERVER_IPV6 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT"
+
 sleep 10
 
 for i in $SERVERS
@@ -11,3 +14,5 @@ do
       "sudo ufw allow from $NEW_SERVER_IPV6 to any port 22 && \
       sudo ufw --force delete \`sudo ufw status numbered | grep $OLD_SERVER_IPV6 | grep -o -E '[0-9]+' | head -1\`"
 done
+
+ssh ubuntu
