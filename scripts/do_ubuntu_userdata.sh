c54f33aa3aec4067424e8cd38f7c0e2b74a9e0d3
danackerson <dan@ackerson.de>
2022-07-15 17:40:53 +0200

    troubleshooting config build yaml 13

--- scripts/do_ubuntu_userdata.sh
+++ scripts/do_ubuntu_userdata.sh
@@ -5,9 +5,6 @@ echo -n "$SERVER_DEPLOY_SECRET_B64" | base64 -d | tee /root/.ssh/id_ed25519
 chmod 400 /root/.ssh/id_ed25519
 echo -n "$SERVER_DEPLOY_PUBLIC_B64" | base64 -d | tee -a /root/.ssh/authorized_keys
 echo -n "$TITAN_PUBLIC_KEY" | tee -a /root/.ssh/authorized_keys
-
-# *.ackerson.de SSL cert
-mkdir /root/traefik
 echo -n "$ACME_JSON_B64" | base64 -d | tee /root/traefik/acme.json
 chmod 600 /root/traefik/acme.json
 
